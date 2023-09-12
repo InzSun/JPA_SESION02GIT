7fbee95d1522b533f014391d2d1f3a4310514005
Fredy Hurtado <alumno@gmail.com>
2023-09-11 21:54:56 -0500

    PROCEDIMIENTO ALMACENADOS

--- src/main/java/main/Prueba.java
+++ src/main/java/main/Prueba.java
@@ -5,8 +5,10 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 import model.TbProducto;
+import model.TbUsuario;
 
 public class Prueba {
 
@@ -16,9 +18,13 @@ public class Prueba {
 		EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("JPA_clase01x");
 		EntityManager pro = fabrica.createEntityManager();
 		
+		Query query = pro.createNativeQuery("{call findAll_usuarios()}", TbUsuario.class);
+		
+		List<TbUsuario> lstUsuarios = query.getResultList();
+		
 		//Vista
-		List<TbProducto> lstpro = pro.createQuery("Select p From TbProducto p", TbProducto.class).getResultList();
-		System.out.println("Nro de Productos:´"+lstpro.size());
+		//List<TbProducto> lstpro = pro.createQuery("Select p From TbProducto p", TbProducto.class).getResultList();
+		System.out.println("Nro de Productos:´"+lstUsuarios.size());
 	}
 
 }
